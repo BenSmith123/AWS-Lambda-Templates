8fd14ac0cfc35202ac35a9f58a3b6fa44f2a9f75
benjiiman12@gmail.com <benjiiman12@gmail.com>
2018-08-26 19:21:40 +1200

    simplified database call

--- database.js
+++ database.js
@@ -4,38 +4,35 @@ const db_credentials = require('./credentials.json');
 async function getUserRecords() {
     return new Promise((resolve, reject) => {
 
-            const connection = mysql.createConnection({
-                host: db_credentials.host,
-                user: db_credentials.user,
-                password: db_credentials.password,
-                database: db_credentials.database
-            });
-  
-            try {
-                connection.connect(function(err) {
+		const connection = mysql.createConnection({
+			host: db_credentials.host,
+			user: db_credentials.user,
+			password: db_credentials.password,
+			database: db_credentials.database
+		});
 
-                    if (!err) {
-                        console.log("Connected!");
+		connection.connect(function(err) {
 
-                        connection.query('SELECT * FROM MY_DATABASE_TABLE', function (error, results) {
-                            if (error) {
-                                connection.destroy();
-                                reject(error)
-                            } else {
-                                connection.end();
-                                resolve(results);
-                            }
-                        });
-                    } else {
-                        console.log("[ERROR] - database CONNECTION failure", err);
-                    }
-                });
+			if (!err) {
+				// connected!
 
-            } catch (error) {
-                console.log("[ERROR]", error)
-            }
+				connection.query('SELECT * FROM MY_DATABASE_TABLE', function (error, results) {
+					if (!error) {
+						// query successful!
+						connection.end();
+						resolve(results);
+					} else {
+						connection.destroy();
+						console.log("[ERROR] - database QUERY failure")
+						// reject(error)
+					}
+				});
+			} else {
+				console.log("[ERROR] - database CONNECTION failure", err);
+			}
+		});
 
-        });
+	});
 
 }
 
